874aa1c0e51d3f83fbbf4b824dc93b742c9a563e
viktor <vktr.buyukli@gmail.com>
2023-06-03 13:30:50 +0300

    update rates tests

--- exchange/tests.py
+++ exchange/tests.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import pathlib
 
+import pytest
 import responses
 from django.conf import settings
 
@@ -11,21 +12,29 @@ from .exchange_provider import (MonoExchange, PrivatExchange, RateAPIExchange,
 root = pathlib.Path(__file__).parent
 
 
+@pytest.fixture
+def mocked():
+    def inner(file_name):
+        return json.load(open(root / "fixtures" / file_name))
+    return inner
+
+
 @responses.activate
-def test_exchange_mono():
-    mocked_response = json.load(open(root / "fixtures/mono_response.json"))
+def test_exchange_mono(mocked):
+    mocked_response = mocked("mono_response.json")
     responses.get(
         "https://api.monobank.ua/bank/currency",
         json=mocked_response,
     )
     e = MonoExchange("mono", "USD", "UAH")
     e.get_rate()
+    assert e.pair.buy == 36.65
     assert e.pair.sell == 37.4406
 
 
 @responses.activate
-def test_privat_rate():
-    mocked_response = json.load(open(root / "fixtures/privat_response.json"))
+def test_privat_rate(mocked):
+    mocked_response = mocked("privat_response.json")
     date = datetime.date(2023, 5, 14)
     responses.get(
         f"https://api.privatbank.ua/p24api/exchange_rates?json&date={date.strftime('%d.%m.%Y')}&coursid=11",
@@ -33,44 +42,41 @@ def test_privat_rate():
     )
     e = PrivatExchange("privat", "USD", "UAH", date)
     e.get_rate()
+    assert e.pair.buy == 37.1
     assert e.pair.sell == 37.6
 
 
 @responses.activate
-def test_exchange_universal():
-    mocked_response = json.load(open(root / "fixtures/universal_response.json"))
+def test_exchange_universal(mocked):
+    mocked_response = mocked("universal_response.json")
     responses.get(
         "https://www.universalbank.com.ua/api/rates/json",
         json=mocked_response,
     )
     e = UniversalExchange("universal", "USD", "UAH")
     e.get_rate()
+    assert e.pair.buy == 37.05
     assert e.pair.sell == 37.55
 
 
 @responses.activate
-def test_vkurse_rate():
-    mocked_response = json.load(open(root / "fixtures/vkurse_response.json"))
+def test_vkurse_rate(mocked):
+    mocked_response = mocked("vkurse_response.json")
     responses.get(
         "https://vkurse.dp.ua/course.json",
         json=mocked_response,
     )
     e = VkurseExchange("vkurse", "USD", "UAH")
     e.get_rate()
+    assert e.pair.buy == 37.6
     assert e.pair.sell == 37.8
 
 
 @responses.activate
-def test_rateapi_rate():
-    mocked_response_uah = json.load(
-        open(root / "fixtures/rateapi_response_uah_response.json")
-    )
-    mocked_response_usd = json.load(
-        open(root / "fixtures/rateapi_response_usd_response.json")
-    )
-    mocked_response_eur = json.load(
-        open(root / "fixtures/rateapi_response_eur_response.json")
-    )
+def test_rateapi_rate(mocked):
+    mocked_response_uah = mocked("rateapi_response_uah_response.json")
+    mocked_response_usd = mocked("rateapi_response_usd_response.json")
+    mocked_response_eur = mocked("rateapi_response_eur_response.json")
 
     api_key = settings.RATE_API_KEY
     responses.get(
@@ -88,4 +94,5 @@ def test_rateapi_rate():
 
     e = RateAPIExchange("rateapi", "USD", "UAH")
     e.get_rate()
+    assert e.pair.buy == 36.9355
     assert e.pair.sell == 36.9413
