8497152af88689eed3b922d60441de1c0740212a
viktor <vktr.buyukli@gmail.com>
2023-08-13 14:01:16 +0300

    dockerfile and docker-compose

--- exchange_rates/settings.py
+++ exchange_rates/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = "exchange_rates.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
